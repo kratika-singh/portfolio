ffcb4c17601050864325d436ee32ba62c1965162
kratika-singh <105146516+kratika-singh@users.noreply.github.com>
2024-07-15 19:58:10 +0530

    qualification

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/" className="home__social-icon" target='_blank'>
+        <a href="https://www.instagram.com/_kratikaaa__" className="home__social-icon" target='_blank'>
             <i className="uil uil-instagram"></i>
         </a>
         <a href="https://www.linkedin.com/in/kratika-singh-/" className="home__social-icon" target='_blank'>
---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,109 @@
+import React,{useState} from "react"
+import "./qualification.css";
+const Qualification = () => {
+  const [ toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) =>{
+    setToggleState(index);
+  }
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My personal Journey</span>
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div className={toggleState === 1?"qualification__button qualification__active button--flex" :"qualification__button button--flex" }
+          onClick={()=>toggleTab(1)}>
+            <i className="uil uil-graduation-cap  qualification__icon"></i>
+            Education
+          </div>
+
+          <div className={toggleState === 2?"qualification__button qualification__active button--flex" :"qualification__button button--flex" }
+           onClick={()=>toggleTab(2)}>
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div className={toggleState === 1?"qualification__content qualification__content-active" :"qualification__content"} >
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">
+                  Masters of Computer Applications
+                </h3>
+                <span className="qualification__subtitle">
+                  Indira Gandhi Delhi Technical University for Women</span>
+                  <div className="qualification__calender">
+                    <i className="uil uil-calendar-alt"></i> 2023-present
+                  </div>
+                
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              
+              <div>
+                <h3 className="qualification__title">
+                  B.Sc.(Hons.) Computer Applications
+                </h3>
+                <span className="qualification__subtitle">
+                  Aligarh Muslim University</span>
+                  <div className="qualification__calender">
+                    <i className="uil uil-calendar-alt"></i> 2020-2023
+                  </div>
+                  <div>
+              </div>
+              </div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line special"></span>
+              <div></div>
+                
+            </div>
+           
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">
+                Intermediate
+                </h3>
+                <span className="qualification__subtitle">
+                St. Fidelis Sr. Sec. School,Aligarh</span>
+                  <div className="qualification__calender">
+                    <i className="uil uil-calendar-alt"></i> 2018-2020
+                  </div>
+                
+              </div>
+            </div>
+          </div>
+
+          <div className={toggleState === 2?"qualification__content qualification__content-active" :"qualification__content"} >
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">
+                  Fresher
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
