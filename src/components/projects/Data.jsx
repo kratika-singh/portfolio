188d495979bf70ea56a0a8ec45a1109510f53c57
kratika-singh <105146516+kratika-singh@users.noreply.github.com>
2024-07-24 13:24:52 +0530

    project updated

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,6 +1,6 @@
 import project1 from "../../assets/project1.jpg"
 import project2 from "../../assets/project2.jpg"
-import project3 from "../../assets/project4.jpg"
+import project3 from "../../assets/project3.jpg"
 import project4 from "../../assets/project4.jpg"
 
 
